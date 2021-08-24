389df76babd13912e8fa7804c9960797549f6a28
Naimur Rahaman <2015naim@gmail.com>
2021-08-24 06:06:32 +0600

    first commit

---
+++ dist/first.js
@@ -0,0 +1,6 @@
+"use strict";
+var num = 50;
+num = 80;
+console.log(num);
+var sum = function (x, y) { return x + y; };
+console.log(sum(5, 8));
---
+++ first.ts
@@ -0,0 +1,7 @@
+let num: number = 50;
+num = 80;
+console.log(num);
+
+const sum = (x: number, y: number) => x + y;
+console.log(sum(5, 8));
+
